36a80bfeff837c05d670f6328379c33b792f99a7
Gabriel Bernardo <bielcotrimsv@gmail.com>
2022-06-28 16:20:09 -0300

    resolução do requisito 5

--- src/App.js
+++ src/App.js
@@ -20,6 +20,23 @@ class App extends React.Component {
     };
   }
 
+  isSaveButtonDisabled = () => {
+    const { cardName, cardDescription, cardImage, cardRare,
+      cardAttr1, cardAttr2, cardAttr3 } = this.state;
+
+    const maxValuePerAttribute = 90;
+    const maxValue = 210;
+
+    const tests1 = [cardName, cardDescription, cardImage, cardRare];
+    const tests2 = [cardAttr1, cardAttr2, cardAttr3];
+
+    if (tests1.some((test) => test === '')) return true;
+    if (tests2.some((test) => test > maxValuePerAttribute
+      || test < 0)) return true;
+    if (parseInt(cardAttr1, 10) + parseInt(cardAttr2, 10)
+      + parseInt(cardAttr3, 10) > maxValue) return true;
+  };
+
   onInputChange = (event) => {
     this.setState({ [event.target.name]: event.target.type === 'checkbox'
       ? event.target.checked : event.target.value });
@@ -40,8 +57,8 @@ class App extends React.Component {
           cardAttr1={ cardAttr1 }
           cardAttr2={ cardAttr2 }
           cardAttr3={ cardAttr3 }
-          /* isSaveButtonDisabled={ isSaveButtonDisabled }
-          onSaveButtonClick={ onSaveButtonClick } */
+          isSaveButtonDisabled={ this.isSaveButtonDisabled() }
+          // onSaveButtonClick={ onSaveButtonClick }
           onInputChange={ (event) => this.onInputChange(event) }
         />
         <Card
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Card.css';
 
 class Card extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2,
       cardAttr3, cardImage, cardRare, cardTrunfo } = this.props;
     return (
-      <>
-        <h4 data-testid="name-card">{ cardName }</h4>
-        <img src={ cardImage } alt={ cardName } data-testid="image-card" />
-        <p data-testid="description-card">{ cardDescription }</p>
-        <span data-testid="attr1-card">{ cardAttr1 }</span>
-        <span data-testid="attr2-card">{ cardAttr2 }</span>
-        <span data-testid="attr3-card">{ cardAttr3 }</span>
-        <span data-testid="rare-card">{ cardRare }</span>
-        { cardTrunfo && (<span data-testid="trunfo-card">Super Trunfo</span>) }
-      </>
+      <div className="card-container">
+        <div className="card">
+          <h3 data-testid="name-card">{ cardName }</h3>
+          <div className="card-information">
+            <img src={ cardImage } alt={ cardName } data-testid="image-card" />
+          </div>
+          <p data-testid="description-card">{ cardDescription }</p>
+          <span data-testid="attr1-card">{ cardAttr1 }</span>
+          <span data-testid="attr2-card">{ cardAttr2 }</span>
+          <span data-testid="attr3-card">{ cardAttr3 }</span>
+          <span data-testid="rare-card">{ cardRare }</span>
+          { cardTrunfo && (<span data-testid="trunfo-card">Super Trunfo</span>) }
+        </div>
+      </div>
     );
   }
 }
